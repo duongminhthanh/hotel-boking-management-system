79d59e4343ef77f317f1f36b231f010dec96a993
duongminhthanh <duongminhthanh1999td@gmail.com>
2024-07-29 16:15:46 +0700

    add CustomUserDetailService.java

---
+++ backend/src/main/java/org/example/backend/service/CustomUserDetailService.java
@@ -0,0 +1,20 @@
+package org.example.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.backend.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(()
+                -> new UsernameNotFoundException("Username/Email not Found"));
+    }
+}
