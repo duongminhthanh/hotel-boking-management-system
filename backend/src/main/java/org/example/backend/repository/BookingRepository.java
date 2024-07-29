13b7166aa070669b44fe24f1fc7a5170d7cc3602
duongminhthanh <duongminhthanh1999td@gmail.com>
2024-07-29 11:23:52 +0700

    add repository

---
+++ backend/src/main/java/org/example/backend/repository/BookingRepository.java
@@ -0,0 +1,12 @@
+package org.example.backend.repository;
+
+import org.example.backend.entities.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BookingRepository extends JpaRepository<Booking,Long> {
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
+}
---
+++ backend/src/main/java/org/example/backend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package org.example.backend.repository;
+
+import org.example.backend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
+}
